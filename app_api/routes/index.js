d0b3878f69a388a2c8752496d50446f72051dd89
Nyzheir Warner <nyzheirwarner@gmail.com>
2026-04-02 21:48:48 -0400

    Module 5 completed baseline

---
+++ app_api/routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+//this is where we import the controllers we will route
+const tripsController = require("../controllers/trips");
+
+// define route for our tirps endpoint
+router
+    .route("/trips")
+    .get(tripsController.tripsList); //Get method routes tripList
+
+router // get method routes tripsFindByCode - requires parameter
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode);
+
+module.exports = router;
